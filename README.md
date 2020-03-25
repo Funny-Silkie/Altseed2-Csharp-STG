b891cf479e971e75ba38257aa4e954f1a22af1b0
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-03-25 19:09:55 +0900

    add roop

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Altseed2 Csharp STG
+
+Altseed2のバージョン：2020/3/25/19:02現在
---
+++ Tutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using Altseed;
+using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Engine.Initialize("Tutorial", 960, 720);
+
+            while (Engine.DoEvents())
+            {
+                if (!Engine.Update()) break;
+            }
+
+            Engine.Terminate();
+        }
+    }
+}
