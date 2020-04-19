d8d0b662e5d26d312582114409319abf8d3c14e7
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-04-19 13:05:05 +0900

    divide chap.02

---
+++ Chapter02/Program.cs
@@ -0,0 +1,39 @@
+﻿using Altseed;
+using System;
+
+namespace Tutorial
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // エンジンを初期化
+            Engine.Initialize("Tutorial", 960, 720, new Configuration());
+
+            // 自機
+            var player = new SpriteNode();
+            // 自機のテクスチャを読み込む
+            player.Texture = Texture2D.LoadStrict("Resources/Player.png");
+
+            // 自機をエンジンに追加
+            Engine.AddNode(player);
+
+            // メインループ
+            while (Engine.DoEvents())
+            {
+                // エンジンを更新
+                Engine.Update();
+
+                // Escapeキーでゲーム終了
+                if (Engine.Keyboard.GetKeyState(Keys.Escape) == ButtonState.Push)
+                {
+                    break;
+                }
+            }
+
+            // エンジンの終了処理を行う
+            Engine.Terminate();
+        }
+    }
+}
