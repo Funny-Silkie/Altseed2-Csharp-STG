62273a27eba5ab5a94830ae2aa4d67907ea1dc0f
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-04-19 16:33:46 +0900

    divide chap.09

---
+++ Chapter09/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using Altseed;
+
+namespace Tutorial
+{
+    // 弾のクラス
+    public class Bullet : CollidableObject
+    {
+        // フレーム毎に進む距離
+        private Vector2F velocity;
+
+        // コンストラクタ
+        public Bullet(MainNode mainNpde, Vector2F position, Vector2F velocity) : base(mainNpde, position)
+        {
+            // 衝突判定を行わないように設定
+            doSurvey = false;
+
+            // 弾速を設定
+            this.velocity = velocity;
+
+            // 表示位置をプレイヤーや敵より奥に設定
+            ZOrder--;
+        }
+
+        // フレーム毎に実行
+        protected override void OnUpdate()
+        {
+            // 座標を速度分進める
+            Position += velocity;
+
+            // CollidableObjectのOnUpdateを呼び出す
+            base.OnUpdate();
+
+            // 画面外に出たら自身を削除
+            RemoveMyselfIfOutOfWindow();
+        }
+    }
+}
---
+++ Chapter09/EnemyBullet.cs
@@ -0,0 +1,31 @@
+﻿using Altseed;
+
+namespace Tutorial
+{
+    // 敵の弾のクラス
+    public class EnemyBullet : Bullet
+    {
+        // コンストラクタ
+        public EnemyBullet(MainNode mainNode, Vector2F position, Vector2F velocity) : base(mainNode, position, velocity)
+        {
+            // テクスチャを読み込む
+            Texture = Texture2D.LoadStrict("Resources/Bullet_Red.png");
+
+            // 中心座標を設定
+            CenterPosition = Texture.Size / 2;
+
+            // 半径を設定
+            collider.Radius = Texture.Size.X / 2;
+        }
+
+        // 衝突時に実行
+        protected override void OnCollision(CollidableObject obj)
+        {
+            // 衝突対象がプレイヤーだったらBulletのOnCollisionを実行して削除
+            if (obj is Player)
+            {
+                Parent?.RemoveChildNode(this);
+            }
+        }
+    }
+}
