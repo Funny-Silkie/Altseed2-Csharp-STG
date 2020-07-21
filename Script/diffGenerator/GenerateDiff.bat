cf3c5a378c71bf86d57bc4d3fed804fd52780934
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-07-22 05:27:33 +0900

    fix bat

--- Script/diffGenerator/GenerateDiff.bat
+++ Script/diffGenerator/GenerateDiff.bat
@@ -8,8 +8,8 @@ mkdir Changed
 
 for %%c in (*.cs) do (
 
-..\Script\diffGenerator\Conv.exe %%c Changed\%%a
-..\Script\diffGenerator\Conv.exe ..\%%a\%%c Changed\%%b
+..\Script\diffGenerator\Conv.exe %%c Changed\%%b
+..\Script\diffGenerator\Conv.exe ..\%%a\%%c Changed\%%a
 
 fc /U Changed\%%b Changed\%%a > Changed\%%c.txt
 del Changed\%%a
